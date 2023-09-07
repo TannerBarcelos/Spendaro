d146decdede81c91025bc9afe61bc8b11a01016d
Tanner Barcelos <41309200+TannerBarcelos@users.noreply.github.com>
2023-09-06 23:01:38 -0700

    Update vite.config.ts

--- spendaro/client/vite.config.ts
+++ spendaro/client/vite.config.ts
@@ -11,9 +11,12 @@ export default defineConfig(({ mode }) => {
     plugins: [react()],
     server: {
       host: true,
-      port: parseInt(env.VITE_APP_PORT) || 5173,
+      port: 5173,
       proxy: {
         '/api': {
+          // Dynamic: Used falls back to localhost in development but if using Docker / in containerized mode, it will use the env
+          // and inject the url string of the service name either in a Docker Compose setting OR Kubernetes
+          // this allows users to run Spendaro in various ways based on if they have experience in containzerizarion or not!
           target: env.VITE_PROXY_URL || "http://127.0.0.1:8000",
           changeOrigin: true,
         },

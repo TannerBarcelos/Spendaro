d871aaf8c9a2ea00e4a5349af8c49f464408fc0c
Tanner Barcelos <41309200+TannerBarcelos@users.noreply.github.com>
2023-09-06 22:35:59 -0700

    Update vite.config.ts

--- spendaro/client/vite.config.ts
+++ spendaro/client/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig(({ mode }) => {
+  // Loaded in if overidden in local .env OR if using Docker
   const env = loadEnv(mode, process.cwd(), '')
   return {
     define: {
@@ -13,7 +14,7 @@ export default defineConfig(({ mode }) => {
       port: parseInt(env.VITE_APP_PORT) || 5173,
       proxy: {
         '/api': {
-          target: env.VITE_PROXY_URL || "localhost:8000",
+          target: env.VITE_PROXY_URL || "http://127.0.0.1:8000",
           changeOrigin: true,
         },
       },

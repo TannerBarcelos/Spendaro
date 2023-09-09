54bfb0bee0300bdde6168b204db121cab0bcd40d
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2023-09-09 12:40:55 -0700

    Removing client request as proxy and env settings are confirmed.

--- spendaro/client/src/App.tsx
+++ spendaro/client/src/App.tsx
@@ -1,19 +1,10 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-  const [name] = useState('Spendaro')
-
-  useEffect(() => {
-    fetch(`/api/root/${ name }`)
-      .then((res) => res.json())
-      .then((data) => console.log(data))
-  }, [])
-
-
   return (
     <>
       <div>

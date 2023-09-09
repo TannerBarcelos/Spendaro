a168e6cdd9cbac0d35379324e97aca44cd0b9cfe
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2023-09-08 17:57:08 -0700

    Adding enum for methods

--- spendaro/api/routes/root_routes.py
+++ spendaro/api/routes/root_routes.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from db.connect_db import PGDatabaseConnector
 from controllers.root_controller import RootController
+from routes.route_utils import HTTPMethods
 
 class RootRouter:
     def __init__(self, db: PGDatabaseConnector):
@@ -9,4 +10,4 @@ class RootRouter:
         self.controller = RootController(db)
 
         # Register routes from root controller (controllers are mapped to a specific router which is registered to a top level router)
-        self.router.add_api_route('/', self.controller.read_root, methods=['GET'])
+        self.router.add_api_route('/', self.controller.read_root, methods=[HTTPMethods.GET.value])
---
+++ spendaro/api/routes/route_utils.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+'''
+Figure out where to put this file and how to import it later
+'''
+
+class HTTPMethods(Enum):
+    GET = 'GET'
+    POST = 'POST'
+    PUT = 'PUT'
+    DELETE = 'DELETE'

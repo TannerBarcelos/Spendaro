f74171e512adb71994449619db3e49155523f152
Tanner Barcelos <41309200+TannerBarcelos@users.noreply.github.com>
2023-11-02 22:03:56 -0700

    Update main.py

--- spendaro/api/routes/main.py
+++ spendaro/api/routes/main.py
@@ -3,12 +3,14 @@ from db.connect_db import PGDatabaseConnector
 from routes.root_routes import RootRoutes
 from .lib.prefixes import RoutePrefixes as RP
 
-router: APIRouter = APIRouter()
+# Top level router which will be injected with other routers for each route path i.e /user, /budgets etc
+root_router: APIRouter = APIRouter()
+
+# Db Connection singleton shared across all route classes, which goes into controllers and down into services
 db: PGDatabaseConnector = PGDatabaseConnector()
 
+# Instantiate each route collection instance so we can get its router and hook it into the root apps router
+root_routes = RootRoutes(db)
+
 # Register routes here
-router.include_router(RootRoutes(db).router, prefix=RP.root.value)
-# router.include_router(OtherRoutes(db).router, prefix=RP.other.value)
-# router.include_router(AnotherRoutes(db).router, prefix=RP.another.value)
-# router.include_router(AndAnotherRoutes(db).router, prefix=RP.and_another.value)
-# router.include_router(AndSoOnRoutes(db).router, prefix=RP.and_so_on.value)
+root_router.include_router(root_routes.router, prefix=RP.root.value)
